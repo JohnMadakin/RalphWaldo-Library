3dd32fade7acc00f0ec1822d639c2441a817be41
johnmadakin <edafeadjeke@gmail.com>
2019-06-13 20:38:12 +0100

    remove api-debugger

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -100,9 +100,5 @@ $app->router->group([
     require __DIR__.'/../routes/web.php';
 });
 
-collect(scandir(__DIR__ . '/../config'))->each(function ($item) use ($app) {
-    $app->configure(basename($item, '.php'));
-});
-
 
 return $app;
